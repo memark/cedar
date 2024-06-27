737b473a17f54032d4de10814e91b780cbb60661
Magnus Markling <magnus@markling.com>
2024-06-27 18:57:20 +0200

    extract resolve_integration_test_path()

--- cedar-testing/build.rs
+++ cedar-testing/build.rs
@@ -7,6 +7,10 @@
     named_arguments_used_positionally
 )]
 
+#[path = "src/integration_testing/resolve_integration_test_path.rs"]
+mod resolve_integration_test_path;
+use resolve_integration_test_path::resolve_integration_test_path;
+
 use std::any;
 use std::env;
 use std::error::Error;
@@ -48,34 +52,34 @@ fn create_test_case(test_dir: &str, file_name: &str) -> String {
     )
 }
 
-/// For relative paths, return the absolute path, assuming that the path
-/// is relative to the root of the `CedarIntegrationTests` repo.
-/// For absolute paths, return them unchanged.
-///
-/// # Panics
-///
-/// Panics if the environment variable `CARGO_MANIFEST_DIR` is not set,
-/// and `CEDAR_INTEGRATION_TESTS_PATH` is not set.
-/// `CARGO_MANIFEST_DIR` should be set by Cargo at build-time, but
-/// `CEDAR_INTEGRATION_TESTS_PATH` overrides `CARGO_MANIFEST_DIR`.
-fn resolve_integration_test_path(path: impl AsRef<Path>) -> PathBuf {
-    if path.as_ref().is_relative() {
-        if let Ok(integration_tests_env_var) = env::var("CEDAR_INTEGRATION_TESTS_PATH") {
-            return PathBuf::from(integration_tests_env_var);
-        }
-        let manifest_dir = env::var("CARGO_MANIFEST_DIR")
-            .expect("`CARGO_MANIFEST_DIR` should be set by Cargo at build-time.");
-        let mut full_path = PathBuf::from(manifest_dir.clone());
-        full_path.push("..");
-        // We run `cargo test` for cedar-drt. In that case, CARGO_MANIFEST_DIR will be
-        // `cedar-spec/cedar-drt` and we want `../cedar/cedar-integration-tests`
-        if manifest_dir.ends_with("cedar-drt") {
-            full_path.push("cedar");
-        }
-        full_path.push("cedar-integration-tests");
-        full_path.push(path.as_ref());
-        full_path
-    } else {
-        path.as_ref().into()
-    }
-}
+// /// For relative paths, return the absolute path, assuming that the path
+// /// is relative to the root of the `CedarIntegrationTests` repo.
+// /// For absolute paths, return them unchanged.
+// ///
+// /// # Panics
+// ///
+// /// Panics if the environment variable `CARGO_MANIFEST_DIR` is not set,
+// /// and `CEDAR_INTEGRATION_TESTS_PATH` is not set.
+// /// `CARGO_MANIFEST_DIR` should be set by Cargo at build-time, but
+// /// `CEDAR_INTEGRATION_TESTS_PATH` overrides `CARGO_MANIFEST_DIR`.
+// fn resolve_integration_test_path(path: impl AsRef<Path>) -> PathBuf {
+//     if path.as_ref().is_relative() {
+//         if let Ok(integration_tests_env_var) = env::var("CEDAR_INTEGRATION_TESTS_PATH") {
+//             return PathBuf::from(integration_tests_env_var);
+//         }
+//         let manifest_dir = env::var("CARGO_MANIFEST_DIR")
+//             .expect("`CARGO_MANIFEST_DIR` should be set by Cargo at build-time.");
+//         let mut full_path = PathBuf::from(manifest_dir.clone());
+//         full_path.push("..");
+//         // We run `cargo test` for cedar-drt. In that case, CARGO_MANIFEST_DIR will be
+//         // `cedar-spec/cedar-drt` and we want `../cedar/cedar-integration-tests`
+//         if manifest_dir.ends_with("cedar-drt") {
+//             full_path.push("cedar");
+//         }
+//         full_path.push("cedar-integration-tests");
+//         full_path.push(path.as_ref());
+//         full_path
+//     } else {
+//         path.as_ref().into()
+//     }
+// }
--- cedar-testing/src/integration_testing/mod.rs
+++ cedar-testing/src/integration_testing/mod.rs
@@ -29,11 +29,7 @@ use cedar_policy_core::extensions::Extensions;
 use cedar_policy_core::{jsonvalue::JsonValueWithNoDuplicateKeys, parser};
 use cedar_policy_validator::ValidatorSchema;
 use serde::{Deserialize, Serialize};
-use std::{
-    collections::HashSet,
-    env,
-    path::{Path, PathBuf},
-};
+use std::{collections::HashSet, path::Path};
 
 /// JSON representation of our integration test file format
 #[derive(Debug, Deserialize, Serialize)]
@@ -98,37 +94,8 @@ fn constant_true() -> bool {
     true
 }
 
-/// For relative paths, return the absolute path, assuming that the path
-/// is relative to the root of the `CedarIntegrationTests` repo.
-/// For absolute paths, return them unchanged.
-///
-/// # Panics
-///
-/// Panics if the environment variable `CARGO_MANIFEST_DIR` is not set,
-/// and `CEDAR_INTEGRATION_TESTS_PATH` is not set.
-/// `CARGO_MANIFEST_DIR` should be set by Cargo at build-time, but
-/// `CEDAR_INTEGRATION_TESTS_PATH` overrides `CARGO_MANIFEST_DIR`.
-pub fn resolve_integration_test_path(path: impl AsRef<Path>) -> PathBuf {
-    if path.as_ref().is_relative() {
-        if let Ok(integration_tests_env_var) = env::var("CEDAR_INTEGRATION_TESTS_PATH") {
-            return PathBuf::from(integration_tests_env_var);
-        }
-        let manifest_dir = env::var("CARGO_MANIFEST_DIR")
-            .expect("`CARGO_MANIFEST_DIR` should be set by Cargo at build-time.");
-        let mut full_path = PathBuf::from(manifest_dir.clone());
-        full_path.push("..");
-        // We run `cargo test` for cedar-drt. In that case, CARGO_MANIFEST_DIR will be
-        // `cedar-spec/cedar-drt` and we want `../cedar/cedar-integration-tests`
-        if manifest_dir.ends_with("cedar-drt") {
-            full_path.push("cedar");
-        }
-        full_path.push("cedar-integration-tests");
-        full_path.push(path.as_ref());
-        full_path
-    } else {
-        path.as_ref().into()
-    }
-}
+pub mod resolve_integration_test_path;
+use resolve_integration_test_path::resolve_integration_test_path;
 
 /// Given a `JsonTest`, parse the provided policies file.
 /// # Panics
